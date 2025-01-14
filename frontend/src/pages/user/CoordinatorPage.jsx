bd30504ffe482e8909ba8de7d5f39c226c6b5b65
Sojib001 <sajibbhattacharjee128@gmail.com>
2025-01-15 01:41:08 +0600

    coordinator page fixed

--- frontend/src/pages/user/CoordinatorPage.jsx
+++ frontend/src/pages/user/CoordinatorPage.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import CoordinatorSidebar from '../../components/coordinator/CoordinatorSidebar';
 import GenerateRoutine from '../../components/coordinator/GenerateRoutine';
 import UpdateRoutine from '../../components/coordinator/UpdateRoutine';
 import ViewRoutine from '../../components/coordinator/ViewRoutine';
 
 export default function CoordinatorPage() {
-  return (
-    <div className="flex min-h-screen bg-gray-100">
-      <CoordinatorSidebar />
-      <main className="flex-1 p-6">
-        <Routes>
-          {/* Add index route to display GenerateRoutine by default */}
-          <Route index element={<GenerateRoutine />} />
-          <Route path="generate" element={<GenerateRoutine />} />
-          <Route path="update" element={<UpdateRoutine />} />
-          <Route path="view" element={<ViewRoutine />} />
-        </Routes>
-      </main>
-    </div>
-  );
-}
+    return (
+      <div className="flex min-h-screen bg-gray-100">
+        <CoordinatorSidebar />
+        <main className="flex-1 p-6">
+          <Routes>
+            {/* Redirect from '/coordinator' to '/coordinator/generate' */}
+            <Route index element={<Navigate to="generate" />} />
+            <Route path="generate" element={<GenerateRoutine />} />
+            <Route path="update" element={<UpdateRoutine />} />
+            <Route path="view" element={<ViewRoutine />} />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
